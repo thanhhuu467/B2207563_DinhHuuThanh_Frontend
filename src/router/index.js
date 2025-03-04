20a5ad94ab2c2eb74ecaf553375cfffab1ca6818
thanhhuu467 <165811903+thanhhuu467@users.noreply.github.com>
2025-03-04 18:47:14 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -2,21 +2,27 @@ import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
 
 const routes = [
-    {
-        path: "/",
-        name: "contactbook",
-        component: ContactBook,
-    },
-    {
-    path: '/:pathMatch(.*)*', // Bắt mọi đường dẫn không khớp
-    name: 'notfound',
-    component: () => import('@/views/NotFound.vue') // Load trang NotFound.vue khi không tìm thấy trang
-    },
+  {
+    path: "/",
+    name: "contactbook",
+    component: ContactBook,
+  },
+  {
+    path: "/:pathMatch(.*)*", // Bắt mọi đường dẫn không khớp
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
+  },
+  {
+    path: "/contacts/:id",
+    name: "contact.edit",
+    component: () => import("@/views/ContactEdit.vue"),
+    props: true, // Truyền các biến trong $route.params vào làm props
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes,
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
 });
 
 export default router;
